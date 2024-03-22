85025c909ad529b7ed956801b8a15c955a3ab6e4
Austin James <arjames1128@gmail.com>
2024-03-22 15:47:26 -0500

    Initial project initialization

---
+++ GUI-Lecture/Source/PluginEditor.cpp
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+GUILectureAudioProcessorEditor::GUILectureAudioProcessorEditor (GUILectureAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+    
+    addAndMakeVisible(mainComponent);
+}
+
+GUILectureAudioProcessorEditor::~GUILectureAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void GUILectureAudioProcessorEditor::paint (juce::Graphics& g)
+{
+
+}
+
+void GUILectureAudioProcessorEditor::resized()
+{
+    mainComponent.setBounds(0, 0, getWidth(), getHeight());
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JUCE-GUI-Lesson
+This is my lesson plan for Audio Software Development class at Belmont University. 
+This project demonstrates how to import custom assets and override juce classes to use the custom visual assets.
+
